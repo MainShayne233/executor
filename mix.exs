2dbb69fff2126622271d3a156c4889165b96a81b
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-08 23:40:35 -0400

    makes dogma happy

--- mix.exs
+++ mix.exs
@@ -13,9 +13,9 @@ defmodule Executor.Mixfile do
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test, 
+        "coveralls": :test,
         "coveralls.detail": :test,
-        "coveralls.post": :test, 
+        "coveralls.post": :test,
         "coveralls.html": :test,
       ],
    ]
