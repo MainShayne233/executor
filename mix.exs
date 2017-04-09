0af1cc078c525cbe25195a5c3d65e3e6aa7d3bcb
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-08 22:51:19 -0400

    updates package

--- mix.exs
+++ mix.exs
@@ -3,15 +3,29 @@ defmodule Executor.Mixfile do
 
   def project do
     [app: :executor,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
   def application do
     [extra_applications: [:logger]]
+  end
+
+  defp description do
+    """
+    Allows for code execution in Elixir.
+    """
+  end
+
+  defp package do
+    [
+      licences: ["MIT"],
+    ]
   end
 
   defp deps do
